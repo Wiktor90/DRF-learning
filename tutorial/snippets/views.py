4dc6a099d6f0692cfe34dfdd8f16cd8d248d2a62
Wiktor90 <w-witkowski90@o2.pl>
2022-11-22 13:38:49 +0100

    add api root and endpont for snippet highlight

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,3 +1,8 @@
+from rest_framework.reverse import reverse
+from rest_framework import renderers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from snippets.models import Snippet
 from snippets.permissions import IsOwnerOrReadOnly
 from snippets.serializers import SnippetSerializer
@@ -6,6 +11,14 @@ from snippets.serializers import UserSerializer
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django.contrib.auth.models import User
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        "users": reverse("user-list", request=request, format=format),
+        "snipets": reverse("snipets-list", request=request, format=format),
+    })
 
 
 class UserList(generics.ListAPIView):
@@ -43,3 +56,12 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
