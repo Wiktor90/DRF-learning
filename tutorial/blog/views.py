31e5807d5abe12f58156dab96d383ae727800d6f
Wiktor90 <w-witkowski90@o2.pl>
2022-11-28 15:33:27 +0100

    feed db + some comment ideas

--- tutorial/blog/views.py
+++ tutorial/blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+# Widok:
+# wszyscy autorzy CRUD / lookup - PK / + ile kazdy autor napisal postow i ile komentarzy
+# Wszystkie Posty  - w liscie widok info z posta plus 2 ostatnie komenty
+# widok indiwidualnego posta - z wszystkimi komentarzami.
