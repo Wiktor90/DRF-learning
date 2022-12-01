e69fafe0d0f12cea62d3e29f1f1bfa44b51a7ab7
Wiktor90 <w-witkowski90@o2.pl>
2022-12-01 22:33:01 +0100

    API plan

--- tutorial/blog/views.py
+++ tutorial/blog/views.py
@@ -2,6 +2,24 @@ from django.shortcuts import render
 
 # Create your views here.
 # Widok:
+
+# AUTHOR
+# /author - autor list + coment_count / post_count - ile czego pisal
+# albo serwis w widoku albo method field w serializerze ?
+# /author/<int:pk> - to samo tylko details view
+
+# POST
+# /author/<int:pk>/post - wszystkie posty per autor. Dodac licznik komentarzy "comments"
+# /author/<int:pk>/post/<post_pk> - konkretny post danego autora. Dodac licznik komentarzy "comments"
+# *zcustomizowac get_object() w widoku
+# /recent/post - wyswietla 5 ostatnich postow. Dodac tu "recent_comments" - 2 najnowsze komentarze posta
+
+# COMMENTS
+# /comment - wszystkie komentarz - post wyswietlac po tytule i autora po name.
+# /author/<int:pk>/comment - wszystkie komenty per autor. wyswietlanie jak wyzej
+
+
+
 # wszyscy autorzy CRUD / lookup - PK / + ile kazdy autor napisal postow i ile komentarzy
 # Wszystkie Posty  - w liscie widok info z posta plus 2 ostatnie komenty
 # widok indiwidualnego posta - z wszystkimi komentarzami.
